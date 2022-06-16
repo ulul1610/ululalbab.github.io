236b05dd03eaf32f85c740e62d1a6ff57a0cceba
Ulul Albab <ulul.albab096@gmail.com>
2022-06-16 11:01:17 +0800

    first commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Contact Website"; //letter subject
+	$to = 'ulul.albab096@gmail.com'; // Enter Your E-mail
+	$from = 'admin@ulul-albab.com'; // Admin e-mail
+	$fromName = 'Ulul'; // Your company name
+	$charset = 'UTF-8';
+?>
